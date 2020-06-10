808c455f38f959646c6ccc2144f0c2b24058fbe0
Ezequias Evangelista <ezequias_santos_sp@yahoo.com.br>
2020-06-09 23:18:30 -0300

    Upload do Projeto

---
+++ variaveis.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "domain-name" {
+  default = "pocdeinfra.com"
+}
+
+variable "ssl-arn" {
+  default = "arn:aws:acm:us-east-1:492909730045:certificate/8c1845c3-5ace-4e56-9738-1393967afbe9"
+}
